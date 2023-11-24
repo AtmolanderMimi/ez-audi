5053a20025cdcde7e435fd3f9a003d6073552aca
Atmolander <skylandersmimi@gmail.com>
2023-11-24 15:29:06 -0500

    Removed useless LPcmMetadata

--- src/wav/wav_audio_formats.rs
+++ src/wav/wav_audio_formats.rs
@@ -1,4 +1,4 @@
-use crate::{audio_codecs::{LPcmMetadata, BytesToSamples}, errors::Error, cpal_abstraction::{Samples, SampleMetadata}};
+use crate::{audio_codecs::{LPcm, BytesToSamples}, errors::Error, cpal_abstraction::{Samples, SampleMetadata}};
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum AudioFormat {
@@ -8,13 +8,13 @@ pub enum AudioFormat {
 impl AudioFormat {
     pub fn bytes_to_u8_samples(&self, bytes: &Vec<u8>, metadata: impl Into<SampleMetadata>) -> Error<Samples<u8>> {
         match self {
-            Self::LPcm => LPcmMetadata::bytes_to_u8_samples(bytes, metadata)
+            Self::LPcm => LPcm::bytes_to_u8_samples(bytes, metadata)
         }
     }
 
     pub fn bytes_to_i16_samples(&self, bytes: &Vec<u8>, metadata: impl Into<SampleMetadata>) -> Error<Samples<i16>> {
         match self {
-            Self::LPcm => LPcmMetadata::bytes_to_i16_samples(bytes, metadata)
+            Self::LPcm => LPcm::bytes_to_i16_samples(bytes, metadata)
         }
     }
 }
