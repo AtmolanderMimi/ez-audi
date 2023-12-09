dcdc6719cef6d157b7ead5a679eb9ba2d4f592bf
Atmolander <skylandersmimi@gmail.com>
2023-12-09 17:58:59 -0500

    Revamped player

--- src/bin/player.rs
+++ src/bin/player.rs
@@ -3,22 +3,11 @@ use ez_audi::{public_traits::*, modifiers};
 
 fn main() {
     const FILE_NAME: &str = "test_assets/tanger-ike.wav";
+    const EXACT: bool = false;
     let wav_audio = WavAudio::new(FILE_NAME).unwrap();
 
-    let true_before = std::time::SystemTime::now();
-    let mut player = wav_audio.play_on_default_output(true).unwrap();
-    println!("Getting samples out time {:?}", std::time::SystemTime::elapsed(&true_before));
+    let mut player = wav_audio.play_on_default_output(EXACT).unwrap();
 
-    // Adding motif
-    player.add_modifier(Box::new(modifiers::Loop(20)));
-    println!("TRUE TIME EXACT {:?}", std::time::SystemTime::elapsed(&true_before));
-
-    let true_before = std::time::SystemTime::now();
-    let mut player = wav_audio.play_on_default_output(false).unwrap();
-    println!("Getting samples out time {:?}", std::time::SystemTime::elapsed(&true_before));
-
-    // Adding motif
-    player.add_modifier(Box::new(modifiers::Loop(20)));
-    println!("TRUE TIME NOT EXACT {:?}", std::time::SystemTime::elapsed(&true_before));
-    std::thread::sleep(std::time::Duration::from_secs(111111111111111))
+    player.add_modifier(Box::new(modifiers::Shittify));
+    loop {}
 }
