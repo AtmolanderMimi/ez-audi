78583c59ba5353b1b668361b6ed6641c9704d3a3
Atmolander <skylandersmimi@gmail.com>
2023-12-09 18:01:38 -0500

    Added modifier example to player bin

--- src/bin/player.rs
+++ src/bin/player.rs
@@ -4,10 +4,12 @@ use ez_audi::{public_traits::*, modifiers};
 fn main() {
     const FILE_NAME: &str = "test_assets/tanger-ike.wav";
     const EXACT: bool = false;
+    const VOLUME: f32 = 1.0;
     let wav_audio = WavAudio::new(FILE_NAME).unwrap();
 
     let mut player = wav_audio.play_on_default_output(EXACT).unwrap();
 
-    player.add_modifier(Box::new(modifiers::Shittify));
+    // Add modifiers here
+    player.add_modifier(Box::new(modifiers::Volume(VOLUME)));
     loop {}
 }
